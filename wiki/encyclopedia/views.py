05ac91fcf91386068c6d422109a48af6e5de367a
Raymond Robinson <rayissach@gmail.com>
2023-05-03 18:35:47 -0500

    search function is functionable

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -5,6 +5,9 @@ from django.urls import reverse
 
 from . import util
 
+# title_entries = []
+searches = []
+
 class SearchForm(forms.Form):
     search = forms.CharField(label="")
     
@@ -36,16 +39,35 @@ def search(request):
         
         if form.is_valid():
             searched = form.cleaned_data['search']
-            if searched:
+            print(searched)
+            searches.append(searched)
+            if titles:
+                print(searches)
+                print(titles)
                 return HttpResponseRedirect(reverse("title", args=[searched] ))
             # elif form.filters.contains(titles):
             #     return render(request, "encyclopedia/search.html", {
             #         "entries": titles
             #     })
             else:
-                return render(request, "encyclopedia/index.html", {
-                    "entries": titles,
-                    "form": SearchForm
+                title_list = util.list_entries()
+                print(searches)
+                print(title_list)
+                
+                title_entries = [title_input for title_input in title_list if searches in title_list]
+                
+                # for title_input in title_list:
+                #     if searched.lower() in title_list.lower() or title_list.lower() in searched.lower():
+                #         title_entries.append[title_input]
+                    
+                        
+                # return {"title_entries": title_entries}
+                        
+                
+                return render(request, "encyclopedia/search.html", {
+                    "title_entries": title_entries,
+                    "form": SearchForm(),
+                    "title": searches
                 })
             
         
