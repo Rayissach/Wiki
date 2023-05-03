572e5d4f16d59254791d39d1ca1b849d3f5dcaf4
Raymond Robinson <rayissach@gmail.com>
2023-05-03 15:27:24 -0500

    Django formset for sidebar on index.html

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,11 +1,24 @@
+from django import forms
 from django.shortcuts import render
 
 from . import util
 
+class SearchForm(forms.Form):
+    search = forms.CharField(label="")
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+   
+        self.fields['search'].widget.attrs['class'] = 'search'
+        self.fields['search'].widget.attrs['placeholder'] = 'Search Encyclopedia'
+        self.fields['search'].widget.attrs['name'] = 'q'
+
 
 def index(request):
+    form = SearchForm()
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(),
+        "form": form
     })
     
 def title(request, title):
@@ -13,3 +26,12 @@ def title(request, title):
         "titles": util.get_entry(title)
     })
 
+def search(request):
+    if request.method == "POST":
+        form = SearchForm(request.POST)
+    
+    return render(request, "encyclopedia/index.html", {
+        "form": form,
+        "queried": util.get_entry()
+    })
+        
