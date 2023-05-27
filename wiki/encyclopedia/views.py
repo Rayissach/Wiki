8c079731ba03868254347de0e1d44302a7aaad18
Raymond Robinson <rayissach@gmail.com>
2023-05-27 16:35:18 -0500

    Completed search query and fixed bugs

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -9,18 +9,12 @@ import random
 from . import util
 
 markdowner = Markdown()
-# title_entries = []
-searches = []
 
 class SearchForm(forms.Form):
-    search = forms.CharField(label="")
-    
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['search'].widget.attrs['class'] = 'search'
-        self.fields['search'].widget.attrs['placeholder'] = 'Search Encyclopedia'
-        self.fields['search'].widget.attrs['name'] = 'q'
-        
+    search = forms.CharField(label="", widget=forms.TextInput(
+        attrs={"name": "q", "class": "search", "placeholder": "Search", }
+    ))
+
 class CreateForm(forms.Form):
     create_title = forms.CharField(label="Create New Title:", widget=forms.TextInput(attrs={'class': "new-title", 'placeholder': 'Title'}))
     create_area = forms.CharField(label="Create New Form", widget=forms.Textarea(attrs={'class':'textarea', 'placeholder': 'Create New Entry Page'}))
@@ -51,32 +45,23 @@ def title(request, title):
     })
         
 def search(request):
-    form = SearchForm(request.POST)
-    titles = util.get_entry(form)
+    query = request.GET.get("search", "q")
+    if query is None or query == "":
+        return render(request, "encyclopedia/search.html", {
+            "form": SearchForm(), "found": "", "query": query
+        })
     
-    if request.method == "POST":
-        
-        if form.is_valid():
-            searched = form.cleaned_data['search']
-            searches.append(searched)
-            
-            if titles:
-                return HttpResponseRedirect(reverse("title", args=[searched] ))
-            else:
-                title_list = util.list_entries()
-                title_entries = [title_input for title_input in title_list if searched in title_list]
-                
-                # for title_input in title_list:
-                #     if searched.lower() in title_list.lower() or title_list.lower() in searched.lower():
-                #         title_entries.append[title_input]
-                                  
-                return render(request, "encyclopedia/search.html", {
-                    "title_entries": title_entries,
-                    "form": SearchForm(),
-                    "title": searches
-                })
-    else:
-        return HttpResponseRedirect(reverse("index"))
+    title_entries = util.list_entries()
+    
+    found = [valid_entries for valid_entries in title_entries if query.lower() in valid_entries.lower()]
+    
+    if len(found) == 1:
+        return redirect("title", found[0])
+    
+    return render(request, "encyclopedia/search.html", {
+        "form": SearchForm(), "found": found, "query": query 
+    })
+
     
 def create(request):
     if request.method == "POST":
